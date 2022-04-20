3f0bf25ebf2731ece589b136481d6e4145f3ec8c
Targarien <lucad7@gmail.com>
2022-04-20 02:29:28 +0200

    Created: colors file

---
+++ src/colors.ts
@@ -0,0 +1,18 @@
+import chroma from 'chroma-js';
+// https://gka.github.io/chroma.js/#color-scales
+
+function generateColorPalette(colors : []) : chroma.Scale {
+    // let hues = chroma(colors)
+    return chroma.scale(...colors);
+}
+
+function complementary(color) : string {
+    return chroma(color).set('hsl.h', 180).hex();
+}
+
+function analogous(color, a) : string {
+    return chroma(color).set('hsl.h', a).hex();
+}
+
+// #808000
+// #b4b400
