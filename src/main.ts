2b994f5e906acaaf61d01527fc24381e8b429a0f
LucaDangeloS <l.dangelo@udc.es>
2023-09-24 13:17:47 +0200

    Added wakeOnUsers function to stop pingLoop when no visualizers are connected

--- src/main.ts
+++ src/main.ts
@@ -18,19 +18,44 @@ async function main() {
     server.start();
     await api.waitForToken(state);
     sync.initialize(); // The synchronizer needs a working token for it to properly work
-
+    
     let vizServer: VisualizerServer = createVisualizerServer(state, visualizerPort);
     state.clearVisualizers();
     await state.loadPalettes();
     console.log(state.colorInfo.palettes);
     state.visualizerServerSocket = vizServer;
 
+    // Initialize the API servers
+        // They receive the synchronizer and state
+
     if (verbose) {
         console.log("Ready");
     }
+
+    // set a timeout to terminate the synchronizer if no users are connected
+    setInterval(() => {wakeOnUsers(state, sync, 10000)}, 8000);
 
     // state.syncVisualizers();
     // await delay(8000);
     // sync.terminate();
     // server.stop();
+}
+
+// Function that terminates the synchronizer if no users are connected within a certain time
+async function wakeOnUsers(state: State, synchronizer: Synchronizer, timeout: number) {
+    if (!state.active) {
+        synchronizer.terminate();
+        return
+    } else if (!synchronizer.active && state.visualizers.length > 0) {
+        synchronizer.initialize();
+        return
+    }
+
+    if (synchronizer.active && state.visualizers.length == 0) {
+        if (state.lastConnectedVisualizer != null && state.lastConnectedVisualizer.getTime() + timeout < Date.now()) {
+            synchronizer.terminate();
+        } else if (state.startTime.getTime() + timeout*2 < Date.now()) {
+            synchronizer.terminate();
+        }
+    }
 }
